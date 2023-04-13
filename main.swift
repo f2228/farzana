d956fc28d813545c3e38d143dd34861ae846a936
farzana ahmadi <124640481+f2228@users.noreply.github.com>
2023-04-13 02:47:06 +0000

    file

--- main.swift
+++ main.swift
@@ -1,7 +1,13 @@
-for i in 1...100 {
-    if i % 2 == 0 {
-        print("\(i) is even")
-    } else {
-        print("\(i) is odd")
+func containsPalindrome(_ array: [String]) -> Bool {
+    for string in array {
+        if string == String(string.reversed()) {
+            return true
+        }
     }
+    return false
 }
+
+let array1 = ["racecar", "hello", "world"]
+let array2 = ["apple", "banana", "racecar"]
+print(containsPalindrome(array1)) // Output: true
+print(containsPalindrome(array2)) // Output: true
